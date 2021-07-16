78ab1aa9333c494d9ca1bdddafee7345ad706927
Tony Ding <62191432+djq060912@users.noreply.github.com>
2021-07-16 19:52:14 +0800

    aex-ify

--- aosp_pdx201.mk
+++ aosp_pdx201.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 # Inherit from common.mk
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -27,10 +27,9 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := Sony
 PRODUCT_DEVICE := pdx201
 PRODUCT_MANUFACTURER := Sony
-PRODUCT_NAME := havoc_pdx201
+PRODUCT_NAME := aosp_pdx201
 PRODUCT_MODEL := XQ-AU52
 
-HAVOC_BUILD_TYPE := Unofficial
 TARGET_BOOT_ANIMATION_RES := 1080
 WITH_GAPPS := true
 TARGET_GAPPS_ARCH := arm64
